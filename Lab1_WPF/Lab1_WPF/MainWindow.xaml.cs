1ef26e95afdda4cf07ab049ee8cc1d4ef052a530
IFOP0112003 Кустов Дмитрий Вячеславович <IFOP0112003@sibupk.su>
2018-09-07 17:13:45 +0700

    2

--- Lab1_WPF/Lab1_WPF/MainWindow.xaml.cs
+++ Lab1_WPF/Lab1_WPF/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Lab1_WPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
