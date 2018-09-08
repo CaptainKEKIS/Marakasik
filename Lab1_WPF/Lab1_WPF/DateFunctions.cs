4ce4558bf6c0f8e93f0b80c0e469005994d3f75e
IFOP0112003 Кустов Дмитрий Вячеславович <IFOP0112003@sibupk.su>
2018-09-08 09:03:48 +0700

    b1

---
+++ Lab1_WPF/Lab1_WPF/DateFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1_WPF
+{
+    class DateFunctions
+    {
+        public static bool IsLeapYear(int year)
+        {
+            //какие-то расчеты
+            return true; //true - если год високосный, иначе false
+        }
+
+    }
+}
